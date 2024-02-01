9a7e6e06be8ee57e2be2a984d75a309409539675
LWangllix <vanglikas.lukas@gmail.com>
2024-02-01 15:29:43 +0200

    migration

---
+++ database/migrations/20240201152282_removeAppsFromUsers.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('users', (table) => {
+    table.dropColumn('apps');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('users', (table) => {
+    table.jsonb('apps');
+  });
+};
