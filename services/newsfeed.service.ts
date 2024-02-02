c6ecc759e7f426eebbb5a1420017f129c23ae192
LWangllix <vanglikas.lukas@gmail.com>
2024-02-02 13:00:56 +0200

    refactor

--- services/newsfeed.service.ts
+++ services/newsfeed.service.ts
@@ -1,34 +1,27 @@
 'use strict';
-import { isEmpty } from 'lodash';
 import moleculer, { Context } from 'moleculer';
-import { Action, Service } from 'moleculer-decorators';
+import { Action, Method, Service } from 'moleculer-decorators';
 import { intersectsQuery } from 'moleculer-postgis';
 import { UserAuthMeta } from '../types';
 import { parseToJsonIfNeeded } from '../utils';
-import { Event } from './events.service';
 import { Subscription } from './subscriptions.service';
 
 @Service({
   name: 'newsfeed',
-})
-export default class NewsfeedService extends moleculer.Service {
-  @Action({
-    rest: 'GET /',
-  })
-  async getNewsFeed(ctx: Context<any, UserAuthMeta>): Promise<any> {
-    const { user } = ctx.meta;
 
-    ctx.params.query = parseToJsonIfNeeded(ctx.params.query) || {};
+  scope: {
+    async applyFilters(ctx: Context<any, UserAuthMeta>) {
+      ctx.params.query = parseToJsonIfNeeded(ctx.params.query) || {};
+      const { user } = ctx.meta;
 
-    const subscriptions: Subscription[] = await ctx.call('subscriptions.find', {
-      query: {
-        user: user.id,
-        active: true,
-      },
-      populate: ['geom'],
-    });
+      const subscriptions: Subscription[] = await ctx.call('subscriptions.find', {
+        query: {
+          user: user.id,
+          active: true,
+        },
+        populate: ['geom'],
+      });
 
-    if (!isEmpty(subscriptions)) {
       const subscriptionQuery = subscriptions.map((subscription) => ({
         app: { $in: subscription.apps },
         $raw: intersectsQuery('geom', subscription.geom, 3346),
@@ -40,13 +33,81 @@ export default class NewsfeedService extends moleculer.Service {
       } else {
         ctx.params.query.$or = subscriptionQuery;
       }
-    }
 
-    const events: Event[] = await ctx.call('events.list', {
+      return ctx;
+    },
+  },
+
+  defaultScopes: ['applyFilters'],
+
+  hooks: {
+    before: {},
+  },
+})
+export default class NewsfeedService extends moleculer.Service {
+  @Action({
+    rest: 'GET /',
+  })
+  async list(ctx: Context<any, UserAuthMeta>) {
+    return ctx.call('events.list', {
       ...ctx,
       query: { ...ctx.params.query },
     });
+  }
 
-    return events;
+  @Action()
+  async find(ctx: Context<any, UserAuthMeta>) {
+    return ctx.call('events.find', {
+      ...ctx,
+      query: { ...ctx.params.query },
+    });
+  }
+
+  @Action()
+  async get(ctx: Context<any, UserAuthMeta>) {
+    const { id } = ctx.params;
+    return ctx.call('events.get', {
+      ...ctx,
+      id,
+      query: { ...ctx.params.query },
+    });
+  }
+
+  @Action()
+  async resolve(ctx: Context<any, UserAuthMeta>) {
+    const { id } = ctx.params;
+    return ctx.call('events.resolve', {
+      ...ctx,
+      id,
+      query: { ...ctx.params.query },
+    });
+  }
+
+  @Method
+  async applyFilters(ctx: Context<any, UserAuthMeta>) {
+    ctx.params.query = parseToJsonIfNeeded(ctx.params.query) || {};
+    const { user } = ctx.meta;
+
+    const subscriptions: Subscription[] = await ctx.call('subscriptions.find', {
+      query: {
+        user: user.id,
+        active: true,
+      },
+      populate: ['geom'],
+    });
+
+    const subscriptionQuery = subscriptions.map((subscription) => ({
+      app: { $in: subscription.apps },
+      $raw: intersectsQuery('geom', subscription.geom, 3346),
+    }));
+
+    if (ctx?.params?.query?.$or) {
+      ctx.params.query.$and = [ctx?.params?.query?.$or, { $or: subscriptionQuery }];
+      delete ctx?.params?.query?.$or;
+    } else {
+      ctx.params.query.$or = subscriptionQuery;
+    }
+
+    return ctx;
   }
 }
